0bf82c2e40dda33de83c7e50060906fb4c04d39f
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-07-16 11:06:23 +0200

    feat: Add url parsing

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+const URL = require('url')
 const Transport = require('winston-transport')
 
 const postData = (options, data) => {
   return new Promise((resolve, reject) => {
-    const lib = options.hostname.startsWith('https') ? require('https') : require('http')
+    const lib = options.protocol.startsWith('https') ? require('https') : require('http')
     const request = lib.request(options, (response) => {
       if (response.statusCode < 200 || response.statusCode > 299) {
         reject(new Error(`Request failed with status code ${response.statusCode}`))
       }
-      const body = []
-      response.on('data', chunk => body.push(chunk))
-      response.on('end', () => resolve(body.join('')))
     })
     request.on('error', error => reject(new Error(error.message)))
     request.write(JSON.stringify(data))
@@ -21,21 +19,20 @@ module.exports = class HttpStreamTransport extends Transport {
   constructor (options = {}) {
     super(options)
     this.options = options
-    if (!options.hostname) throw new Error('HttpStreamTransport: Missing hostname')
+    if (!options.url) throw new Error('HttpStreamTransport: Missing URL')
+    const parsedURL = URL.parse(options.url)
+    this.options.hostname = parsedURL.hostname
+    this.options.path = parsedURL.pathname
+    this.options.protocol = parsedURL.protocol
+    this.options.method = 'POST'
   }
 
   log (info, callback) {
     setImmediate(() => {
       this.emit('logged', info)
     })
-
-    // posting data to endpoint
-    // this.on('data', (chunk) => {
-      postData(this.options, info)
-        .then()
-        .catch(error => new Error(error.message))
-    // })
-
-    callback()
+    postData(this.options, info)
+      .then(callback())
+      .catch(error => console.error(`HttpTransportStream: ${error.message}`))
   }
 }
